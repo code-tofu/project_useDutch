df589b822478741d053914ac01fd58d4ae58d1ed
code_tofu <benjamin.ftc@gmail.com>
2024-02-08 01:53:35 +0800

    init

---
+++ test/brainstorm.md
@@ -0,0 +1,28 @@
+```javascript
+const users = [ {name: "A"},{name: "B"},{name: "C"}] //id is the index
+
+const items = [
+    {name:"one", price:"7.99", split: [0.25, 0.5, 0.25]},
+    {name:"two", price:"8.99", split: [0.25, 0.5, 0]},
+    {name:"three", price:"9.99", split: [0.5, 0, 0.5]}] //id is the index
+
+//generate copies each item split into the dutch array
+const dutch = {
+    [{0.25*7.99},{0.25*8.99},{0.5*9.99}],
+    subtotal: 0.25*7.99 + 0.25*8.99 + 0.5*9.99,
+    SVC: 0.1 * (0.25*7.99 + 0.25*8.99 + 0.5*9.99),
+    GST: 0.09 * (0.25*7.99 + 0.25*8.99 + 0.5*9.99 + SVC),
+    total: subtotal + svc + gst,
+    paid: 
+}
+
+27.22 
+
+const settle = [{pay:1,amount:2.50},{pay:2,amount:3.50}]
+
+const round = [{dutch,dutch,dutch}] //corresponds to user index
+
+const party = [{round1,round2,}]
+
+
+```
---
+++ test/index.js
@@ -0,0 +1,48 @@
+var _gst = 0.09
+var _svc = 0.1
+
+function calculateSpent(items, idx) {
+    let spent = [];
+    let subtotal = 0.0;
+    items.forEach(item=> {
+        const cost = item.price * item.split[idx]        
+        spent.push({[item.name]:cost})
+        subtotal += cost;
+    })
+    const svc = _svc * subtotal;
+    const gst = _gst *(subtotal + svc);
+    return {
+        spent:spent,
+        subtotal:subtotal,
+        serviceCharge:svc,
+        tax:gst,
+        total: subtotal+svc+gst
+    };
+}
+
+function generateBill(items,paid,users,len){
+    let bill =[]
+    for(let i=0;i<len;i++){
+        let dutch = calculateSpent(items,i);
+        dutch.paid=paid[i];
+        dutch.name=users[i];
+        bill.push(dutch);
+    }
+    return bill;
+}
+
+const users = ["A","B","C"];
+const paid = [20.00,12.337,0];
+const items = [
+    {name:"dishone", price:7.99, split: [0.25, 0.5, 0.25]},
+    {name:"dishtwo", price:8.99, split: [0.5, 0.5, 0]},
+    {name:"dishthree", price:9.99, split: [0.5, 0, 0.5]}]
+//26.97, 32.337
+console.log("USERS",users)
+console.log("PAID",paid)
+console.log("ITEMS",items)
+console.log(generateBill(items,paid,users,users.length))
+
+
+
+
